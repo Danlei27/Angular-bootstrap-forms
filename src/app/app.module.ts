6ebcae42a16abccaf9cb74fbeea710c7d0c99793
danlei27 <danleisantos@hotmail.com>
2020-05-13 11:45:53 -0300

    (template driven) controles ngForm, ngSubmit e ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DateFormComponent } from './date-form/date-form.component';
@@ -14,7 +15,8 @@ import { TemplateFormComponent } from './template-form/template-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

8f70ab0c0c6fe0068cd1cdb00722bd001bf5cee0
danlei27 <danleisantos@hotmail.com>
2020-05-20 00:38:03 -0300

    Formulários reativos: Validação Customizada (CEP)

--- src/app/date-form/date-form.component.ts
+++ src/app/date-form/date-form.component.ts
@@ -56,7 +56,7 @@ export class DateFormComponent implements OnInit {
       email: [null, [Validators.required, Validators.email]], 
       endereco: this.formBuilder.group({
 
-      cep:[null, [Validators.required]],
+      cep:[null, [Validators.required, FormValidations.cepValidator]],
       numero:[null, [Validators.required]],
       complemento:[null],
       rua:[null, [Validators.required]],
@@ -136,8 +136,15 @@ export class DateFormComponent implements OnInit {
   verificaValidTouched(campo: string){
 
     return !this.formulario.get(campo).valid && (this.formulario.get(campo).touched || this.formulario.get(campo).dirty); 
-  }
+  } 
 
+  verificaRequired(campo: string){
+    return (
+      this.formulario.get(campo).hasError('required') &&
+      (this.formulario.get(campo).touched || this.formulario.get(campo).dirty)
+    )
+  }
+  
   verificaEmailInvalido(){
     let campoEmail = this.formulario.get('email');
     if(campoEmail.errors){
